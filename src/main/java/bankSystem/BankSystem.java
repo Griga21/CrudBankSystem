cced12e5eea1ecf204210013950e3d021d53b089
User <griga.k.r21@gmail.com>
2024-10-02 11:12:50 +0300

    Create main file

---
+++ src/main/java/bankSystem/BankSystem.java
@@ -0,0 +1,11 @@
+package bankSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankSystem {
+    public static void main(String[] args) {
+        SpringApplication.run(BankSystem.class, args);
+    }
+}
