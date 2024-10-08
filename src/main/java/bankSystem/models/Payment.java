0b18e906a030b4436828a3745c915ca5854b822c
User <griga.k.r21@gmail.com>
2024-10-08 15:07:27 +0300

    add variables

--- src/main/java/bankSystem/models/Payment.java
+++ src/main/java/bankSystem/models/Payment.java
@@ -1,7 +1,22 @@
 package bankSystem.models;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.util.Date;
 
 @Entity
+@Table(name = "payments")
 public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long idPayment;
+
+    @ManyToOne(optional = false, fetch = FetchType.EAGER)
+    @Column(name = "client_id")
+    private Long clientId;
+    @Column(name = "credit_id")
+    private Long creditId;
+    @Column(name = "amount")
+    private double amount;
+    @Column(name = "data")
+    private Date date;
 }
